2ddb615b9c09b8a7e48e367680f20735bebea05c
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2024-02-10 17:14:46 +0100

    Navigation de manière dynamique avec des liens "LInk".

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,14 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
 export default function Home() {
+
+  const id = "article"
+
   return (
     <div className={styles.container}>
       <h1>HELLO NEXT JS !</h1>
       
-      <Link href="/blog/article">
+      <Link href={`/blog/${id}`}>
         Blog
       </Link>
     </div>
